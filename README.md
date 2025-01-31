e19d09e169aae6668bcbe9953c534d0752fddc90
ItouRui <itourui.i@icloud.com>
2025-01-31 14:05:49 +0900

    feat: add initial Neovim configuration and plugins
    - Add .gitignore to exclude specific directories
    - Create README.md with installation instructions
    - Add Neovim configuration files and plugins
    - Include LazyVim setup and custom key mappings
    - Add plugins for AI, coding, colorscheme, editor, LSP, treesitter, and
    UI

---
+++ README.md
@@ -0,0 +1,26 @@
+# dotfiles
+
+## Features
+
+This configuration file contains.
+
+- [Nvim](./nvim/README.md)
+
+## Installation
+
+1. [HomeBrew](https://brew.sh/en/)
+
+```sh
+mkdir ~/.homebrew
+```
+
+```sh
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew
+```
+
+2. Nvim dependencies
+
+```sh
+brew install neovim lazygit fzf nodebrew lynx gh tree-sitter git-flow
+```
+
---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,245 @@
+return {
+	{
+		enabled = false,
+		"folke/flash.nvim",
+		---@type table
+		opts = {
+			search = {
+				forward = true,
+				multi_window = false,
+				wrap = false,
+				incremental = true,
+			},
+		},
+	},
+
+	{
+		"echasnovski/mini.hipatterns",
+		event = "BufReadPre",
+		opts = {
+			highlighters = {
+				hsl_color = {
+					pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
+					group = function(_, match)
+						local utils = require("solarized-osaka.hsl")
+						--- @type string, string, string
+						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
+						--- @type number?, number?, number?
+						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+						--- @type string
+						local hex_color = utils.hslToHex(h or 0, s or 0, l or 0)
+						return require("mini.hipatterns").compute_hex_color_group(hex_color, "bg")
+					end,
+				},
+			},
+		},
+	},
+
+	{
+		"dinhhuy258/git.nvim",
+		event = "BufReadPre",
+		opts = {
+			keymaps = {
+				-- Open blame window
+				blame = "<Leader>gb",
+				-- Open file/folder in git repository
+				browse = "<Leader>go",
+			},
+		},
+	},
+
+	{
+		"pwntester/octo.nvim",
+		opts = function(_, opts)
+			opts.enable_builtin = true
+			opts.default_to_projects_v2 = true
+			opts.default_merge_method = "squash"
+			opts.picker = "fzf-lua"
+
+			if LazyVim.has("fzf-lua") then
+				opts.picker = "fzf-lua"
+			elseif LazyVim.has("telescope.nvim") then
+				opts.picker = "telescope"
+			else
+				LazyVim.error("`octo.nvim` requires `telescope.nvim` or `fzf-lua`")
+			end
+
+			-- Keep some empty windows in sessions
+			vim.api.nvim_create_autocmd("ExitPre", {
+				group = vim.api.nvim_create_augroup("octo_exit_pre", { clear = true }),
+				callback = function(ev)
+					local keep = { "octo" }
+					for _, win in ipairs(vim.api.nvim_list_wins()) do
+						local buf = vim.api.nvim_win_get_buf(win)
+						if vim.tbl_contains(keep, vim.bo[buf].filetype) then
+							vim.bo[buf].buftype = "" -- set buftype to empty to keep the window
+						end
+					end
+				end,
+			})
+		end,
+	},
+
+	{
+		"telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		keys = {
+			{
+				"<leader>fP",
+				function()
+					require("telescope.builtin").find_files({
+						cwd = require("lazy.core.config").options.root,
+					})
+				end,
+				desc = "Find Plugin File",
+			},
+			{
+				";f",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.find_files({
+						no_ignore = false,
+						hidden = true,
+					})
+				end,
+				desc = "Lists files in your current working directory, respects .gitignore",
+			},
+			{
+				";r",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.live_grep({
+						additional_args = { "--hidden" },
+					})
+				end,
+				desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
+			},
+			{
+				"\\\\",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.buffers()
+				end,
+				desc = "Lists open buffers",
+			},
+			{
+				";t",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.help_tags()
+				end,
+				desc = "Lists available help tags and opens a new window with the relevant help info on <cr>",
+			},
+			{
+				";;",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.resume()
+				end,
+				desc = "Resume the previous telescope picker",
+			},
+			{
+				";e",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.diagnostics()
+				end,
+				desc = "Lists Diagnostics for all open buffers or a specific buffer",
+			},
+			{
+				";s",
+				function()
+					local builtin = require("telescope.builtin") ---@type table
+					builtin.treesitter()
+				end,
+				desc = "Lists Function names, variables, from Treesitter",
+			},
+			{
+				"sf",
+				function()
+					local telescope = require("telescope")
+
+					local function telescope_buffer_dir()
+						return vim.fn.expand("%:p:h")
+					end
+
+					telescope.extensions.file_browser.file_browser({
+						path = "%:p:h",
+						cwd = telescope_buffer_dir(),
+						respect_gitignore = false,
+						hidden = true,
+						grouped = true,
+						previewer = false,
+						initial_mode = "normal",
+						layout_config = { height = 40 },
+					})
+				end,
+				desc = "Open File Browser with the path of the current buffer",
+			},
+		},
+		config = function(_, opts)
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local fb_actions = require("telescope._extensions.file_browser.actions")
+
+			opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+				wrap_results = true,
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
+				mappings = {
+					n = {},
+				},
+			})
+			opts.pickers = {
+				diagnostics = {
+					theme = "ivy",
+					initial_mode = "normal",
+					layout_config = {
+						preview_cutoff = 9999,
+					},
+				},
+			}
+			opts.extensions = {
+				file_browser = {
+					theme = "dropdown",
+					-- disables netrw and use telescope-file-browser in its place
+					hijack_netrw = true,
+					mappings = {
+						-- your custom insert mode mappings
+						["n"] = {
+							-- your custom normal mode mappings
+							["N"] = fb_actions.create,
+							["h"] = fb_actions.goto_parent_dir,
+							["/"] = function()
+								vim.cmd("startinsert")
+							end,
+							["<C-u>"] = function(prompt_bufnr)
+								for i = 1, 10 do
+									actions.move_selection_previous(prompt_bufnr)
+								end
+							end,
+							["<C-d>"] = function(prompt_bufnr)
+								for i = 1, 10 do
+									actions.move_selection_next(prompt_bufnr)
+								end
+							end,
+							["<PageUp>"] = actions.preview_scrolling_up,
+							["<PageDown>"] = actions.preview_scrolling_down,
+						},
+					},
+				},
+			}
+			telescope.setup(opts)
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("file_browser")
+		end,
+	},
+}
