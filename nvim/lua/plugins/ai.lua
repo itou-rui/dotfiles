c6fedb351442da1c0e2cc15d212befc3fa1e4339
ItouRui <itourui.i@icloud.com>
2025-02-17 20:25:20 +0900

    feat(ai.lua): add dependencies and build step
    - Added dependencies for GitHub Copilot and plenary.nvim
    - Included build step for tiktoken on MacOS or Linux

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -10,6 +10,12 @@ return {
 		separator = "---",
 		error_header = "> [!ERROR] Error",
 
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+
 		opts = function(_, options)
 			local select = require("CopilotChat.select")
 			local cached_gitdiff = nil
