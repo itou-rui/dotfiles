c18d77990ccc45432a3c1fae43e59829d270fc5a
ItouRui <itourui.i@icloud.com>
2025-03-11 10:20:39 +0900

    feat: add completeopt settings for older Neovim versions
    Added a conditional check to append 'noinsert' and 'popup' to the
    completeopt setting for Neovim versions older than 0.11.0.
    
    This ensures better completion behavior in older versions.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -47,3 +47,7 @@ vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
 if vim.fn.has("nvim-0.8") == 1 then
 	vim.opt.cmdheight = 0
 end
+
+if vim.fn.has("nvim-0.11.0") == 0 then
+	vim.opt.completeopt:append({ "noinsert", "popup" })
+end
