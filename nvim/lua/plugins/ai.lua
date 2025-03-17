a2d5f1ac9cf1f02065567d59e1c525082d40ca0e
ItouRui <itourui.i@icloud.com>
2025-03-17 10:44:53 +0900

    feat: remove lmstudio provider configuration
    The lmstudio provider configuration has been removed from the ai.lua
    plugin file.
    
    This includes the removal of functions for preparing input and output,
    fetching models, embedding inputs, and getting the URL for chat
    completions.

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -77,53 +77,6 @@ return {
 						return "http://localhost:11434/v1/chat/completions"
 					end,
 				},
-
-				lmstudio = {
-					prepare_input = require("CopilotChat.config.providers").copilot.prepare_input,
-					prepare_output = require("CopilotChat.config.providers").copilot.prepare_output,
-
-					get_models = function(headers)
-						local response, err = require("CopilotChat.utils").curl_get("http://localhost:1234/v1/models", {
-							headers = headers,
-							json_response = true,
-						})
-
-						if err then
-							error(err)
-						end
-
-						return vim.tbl_map(function(model)
-							return {
-								id = model.id,
-								name = model.id,
-							}
-						end, response.body.data)
-					end,
-
-					embed = function(inputs, headers)
-						local response, err =
-							require("CopilotChat.utils").curl_post("http://localhost:1234/v1/embeddings", {
-								headers = headers,
-								json_request = true,
-								json_response = true,
-								body = {
-									dimensions = 512,
-									input = inputs,
-									model = "text-embedding-nomic-embed-text-v1.5",
-								},
-							})
-
-						if err then
-							error(err)
-						end
-
-						return response.body.data
-					end,
-
-					get_url = function()
-						return "http://localhost:1234/v1/chat/completions"
-					end,
-				},
 			}
 
 			options.prompts = {
