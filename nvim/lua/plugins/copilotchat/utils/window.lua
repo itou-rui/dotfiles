aa3ca9251022a11732766dfba9ab2e290855624e
ItouRui <itourui.i@icloud.com>
2025-05-25 22:57:42 +0900

    refactor: update window utils for draft prompt support
    Extend window utility functions to support an optional
    `draft_prompt` field in options. Add logic to set the draft prompt
    and handle sticky prompts when opening CopilotChat windows.
    Update type annotations to reflect the new `WindowOpts` structure.

--- nvim/lua/plugins/copilotchat/utils/window.lua
+++ nvim/lua/plugins/copilotchat/utils/window.lua
@@ -1,5 +1,8 @@
 local copilotchat = require("CopilotChat")
 
+---@class WindowOpts: CopilotChat.config
+---@field draft_prompt string|nil
+
 local M = {}
 
 M.float_window = {
@@ -24,18 +27,34 @@ M.clear = function()
 end
 
 ---@param prompt string | nil
----@param opts CopilotChat.config
+---@param opts WindowOpts
 M.open = function(prompt, opts)
 	M.clear()
 	if prompt and prompt ~= "" then
 		copilotchat.ask(prompt, opts)
 	else
 		copilotchat.open(opts)
+		if opts.draft_prompt and opts.draft_prompt ~= "" then
+			vim.schedule(function()
+				vim.cmd("normal! gv")
+				copilotchat.chat:set_prompt(opts.draft_prompt)
+				vim.schedule(function()
+					if opts.sticky then
+						local stickies = type(opts.sticky) == "table" and opts.sticky or { opts.sticky }
+						for _, sticky in ipairs(stickies) do
+							if sticky and sticky ~= "" then
+								copilotchat.chat:add_sticky(sticky)
+							end
+						end
+					end
+				end)
+			end)
+		end
 	end
 end
 
 ---@param prompt string
----@param opts CopilotChat.config
+---@param opts WindowOpts
 M.open_float = function(prompt, opts)
 	opts = opts or {}
 	opts.window = M.float_window
@@ -43,26 +62,26 @@ M.open_float = function(prompt, opts)
 end
 
 ---@param prompt string
----@param opts CopilotChat.config
+---@param opts WindowOpts
 M.open_vertical = function(prompt, opts)
 	opts = opts or {}
 	opts.window = { layout = "vertical" }
 	M.open(prompt, opts)
 end
 
----@param opts CopilotChat.config
+---@param opts WindowOpts
 M.toggle = function(opts)
 	copilotchat.open(opts)
 end
 
----@param opts CopilotChat.config
+---@param opts WindowOpts
 M.toggle_float = function(opts)
 	opts = opts or {}
 	opts.window = M.float_window
 	M.toggle(opts)
 end
 
----@param opts CopilotChat.config
+---@param opts WindowOpts
 M.toggle_vertical = function(opts)
 	opts = opts or {}
 	opts.window = { layout = "vertical" }
